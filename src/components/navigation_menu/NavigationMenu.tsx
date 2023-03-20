a277587ece96b1f8c69bb69ef9dbebcb0a6fd6ea
Elias Q. Castro <40774725+eliasqcastro@users.noreply.github.com>
2023-03-19 22:12:02 -0300

    Creating the navigation menu
    In this update I created the navigation menu and made some minor changes to the general code.

---
+++ src/components/navigation_menu/NavigationMenu.tsx
@@ -0,0 +1,13 @@
+import './NavigationMenu.css'
+
+function NavigationMenu() {
+
+    return (
+
+        <nav className='menu'>
+            <a className='menu__item'>HOME</a>
+        </nav>
+    )
+}
+
+export default NavigationMenu;
